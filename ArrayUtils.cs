a4855cf0568a419166cc18aefca0d1064e058c43
DESKTOP-UJRR6FS\Admin <tuduchoang@gmail.com>
2023-04-04 19:09:14 +0700

    1.0.0

---
+++ ArrayUtils.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Arrays
+{
+    public static class ArrayUtils
+    {
+        public static void Copy<T>(ref T[] array, int length, Func<int, T> selector)
+        {
+            for (var i = 0; i < length; i++)
+            {
+                array[i] = selector(i);
+            }
+        }
+    }
+}
