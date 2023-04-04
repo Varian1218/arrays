24bc64ad27008006052816d38fd93afd1f992587
DESKTOP-UJRR6FS\Admin <tuduchoang@gmail.com>
2023-04-04 19:22:47 +0700

    1.0.1

--- ArrayUtils.cs
+++ ArrayUtils.cs
@@ -11,5 +11,13 @@ namespace Arrays
                 array[i] = selector(i);
             }
         }
+        
+        public static void Copy<T>(T[] array, int length, Func<int, T> selector)
+        {
+            for (var i = 0; i < length; i++)
+            {
+                array[i] = selector(i);
+            }
+        }
     }
 }
