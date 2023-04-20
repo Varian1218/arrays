38f66a55b700e9bade160b9666c0a5fb987b1671
hoang <hoang.tu@incepit.com>
2023-04-20 14:22:15 +0700

    2.8.0

--- RandomUtils.cs
+++ RandomUtils.cs
@@ -14,5 +14,23 @@ namespace CSharpExtensions
 
             return ts;
         }
+        
+        public static T[] Next<T>(int count, Func<int, T> next)
+        {
+            var ts = new T[count];
+            for (var i = 0; i < count; i++)
+            {
+                ts[i] = next(i);
+            }
+
+            return ts;
+        }
+
+        public static byte[] NextBytes(int count, Random random)
+        {
+            var bytes = new byte[count];
+            random.NextBytes(bytes);
+            return bytes;
+        }
     }
 }
