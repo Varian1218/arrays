9dc358e137b76cd341467a26bab109a690c3f132
DESKTOP-UJRR6FS\Admin <tuduchoang@gmail.com>
2023-04-23 12:06:59 +0700

    2.9.0

---
+++ Locker.cs
@@ -0,0 +1,19 @@
+﻿namespace CSharpExtensions
+{
+    public class Locker
+    {
+        private bool _locked;
+
+        public bool Lock()
+        {
+            if (_locked) return false;
+            _locked = true;
+            return true;
+        }
+
+        public void Unlock()
+        {
+            _locked = false;
+        }
+    }
+}
