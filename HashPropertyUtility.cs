cd4e8a5a99d8963bdcdb26f1ee199ff226641312
hoangtuincepit <128338344+hoangtuincepit@users.noreply.github.com>
2023-05-04 11:20:25 +0700

    3.6.0

--- HashPropertyUtility.cs
+++ HashPropertyUtility.cs
@@ -7,11 +7,22 @@ namespace CSharpBoosts
 {
     public static class HashPropertyUtility
     {
-        public static IEnumerable<(string, PropertyInfo)> GetProperties(Type type)
+        public static IEnumerable<(string Hash, PropertyInfo Info)> GetProperties<T>()
+        {
+            return GetProperties(typeof(T));
+        }
+
+        public static IEnumerable<(string Hash, PropertyInfo Info)> GetProperties(Type type)
+        {
+            return GetProperties<HashPropertyAttribute>(type).Select(it => (it.Attribute.Hash, it.Info));
+        }
+
+        public static IEnumerable<(TAttribute Attribute, PropertyInfo Info)> GetProperties<TAttribute>(Type type)
+            where TAttribute : Attribute
         {
             return type.GetProperties(BindingFlags.Instance | BindingFlags.Public)
-                .Select(it => (it.GetCustomAttribute<HashPropertyAttribute>()?.Hash, it))
-                .Where(it => !string.IsNullOrEmpty(it.Hash));
+                .Select(it => (it.GetCustomAttribute<TAttribute>(), it))
+                .Where(it => it.Item1 != null);
         }
     }
 }
