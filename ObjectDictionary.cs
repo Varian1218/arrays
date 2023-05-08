b79e43d95c25bf5742dd218627ae37d0142281d1
hoangtuincepit <128338344+hoangtuincepit@users.noreply.github.com>
2023-05-08 10:08:34 +0700

    3.7.0

---
+++ ObjectDictionary.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections.Generic;
+
+namespace CSharpBoosts
+{
+    public class ObjectDictionary<TObject>
+    {
+        private Dictionary<string, TObject> _objects;
+
+        public void Add(Type type, TObject obj)
+        {
+            if (!type.IsInstanceOfType(obj)) throw new ArgumentException(type.FullName, obj.GetType().FullName);
+            _objects.Add(GetHash(type), obj);
+        }
+
+        public void AddMany(IEnumerable<(Type type, TObject obj)> objects)
+        {
+            foreach (var (type, obj) in objects)
+            {
+                Add(type, obj);
+            }
+        }
+
+        public void Add<T>(T obj) where T : TObject
+        {
+            Add(typeof(T), obj);
+        }
+
+        public TObject Get(Type type)
+        {
+            return _objects[GetHash(type)];
+        }
+
+        public T Get<T>() where T : TObject
+        {
+            return (T)Get(typeof(T));
+        }
+
+        private static string GetHash(Type type)
+        {
+            return type.FullName;
+        }
+    }
+}
