881617f4b87e1edc868e9edf3a7faf80f3eb2ad5
hoangtuincepit <128338344+hoangtuincepit@users.noreply.github.com>
2023-05-12 16:35:21 +0700

    3.13.0

--- Abilities/Factories/AbilityFactoryUtility.cs
+++ Abilities/Factories/AbilityFactoryUtility.cs
@@ -1,5 +1,7 @@
 using System;
+using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using CSharpBoosts;
 using Factories;
 
@@ -7,6 +9,9 @@ namespace Abilities.Factories
 {
     public static class AbilityFactoryUtility
     {
+        public static IEnumerable<string> All => TypeUtils.All
+            .SelectNotNull(it => it.GetCustomAttribute<AbilityFactoryAttribute>()).Select(it => it.Hash);
+
         public static IFactory<AbilityMaterial<THandler>, IAbilityExtension<THandler>> Create<THandler>()
         {
             return TypeUtils.All.GetHashTypes<AbilityFactoryAttribute>()
